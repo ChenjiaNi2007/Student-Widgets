afaf8801372e1f7d09a5d95ddb6474312719751b
Chenjia <chenjiani2007@gmail.com>
2020-11-26 22:32:03 -0800

    saving notes local storage

--- notepad/script.js
+++ notepad/script.js
@@ -2,6 +2,17 @@ const titleInput = document.querySelector('#titled');
 const bodyinput = document.querySelector('#body');
 const userlist = document.querySelector('#notes');
 const div = document.querySelector('#div')
+const reset = document.querySelector('.reset')
+
+if(localStorage.getItem("number") == null){
+	localStorage.setItem("number","0")
+}
+for(let i = 1; i <= localStorage.getItem("number"); i++){
+	const li = document.createElement('li'); 
+ 	li.appendChild(document.createTextNode(localStorage.getItem(String(i))));
+ 	li.classList.add('added');
+  	userlist.appendChild(li);
+}
 
 function add_on() {
   
@@ -10,6 +21,14 @@ function add_on() {
   li.classList.add('added');
   userlist.appendChild(li);
 
+  localStorage.setItem("number", String(Number(localStorage.getItem("number"))+1))
+  localStorage.setItem(localStorage.getItem("number"), `${titleInput.value}: ${bodyinput.value}`);
+
   titleInput.value = '';
   bodyinput.value = '';
+}
+
+reset.onclick = function() {
+	localStorage.clear();
+	document.querySelector("ul").innerHTML = "";
 }
